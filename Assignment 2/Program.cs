5f4ff947ae9947102305c0749b3a0c5129a0073c
Sinates <sinatesfaye38@gmail.com>
2022-06-28 19:52:56 -0700

    first commit

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int Secretnumber = 3;
+            int Guess= 0;
+            int GuessCount = 0;
+
+            while (GuessCount < 3 )
+               
+            {
+              
+                Console.WriteLine("Guess a number");
+                Guess = Convert.ToInt32(Console.ReadLine());
+
+                if (Secretnumber == Guess)
+                {
+                    Console.WriteLine("You got it");
+                    return;
+
+
+                }
+                GuessCount++;
+            }
+            Console.WriteLine("You lost");
+
+
+        }
+        }
+    }
